59907b78209ecc7d1d120d2a039ffcaaa67dca46
Dirk Lehmann <develop@dj-l.de>
2019-07-01 00:41:36 +0200

    makeinc/makefile.regex.mk: Forgotten to remove an unused variable ...

--- makeinc/makefile.regex.mk
+++ makeinc/makefile.regex.mk
@@ -27,9 +27,6 @@ _REGEX_DIRNAME_RET = 's~^\(.*\)/[^/]\+~\1~;'
 # sed_cmd _REGEX_DOS2UNIX_NL
 _REGEX_DOS2UNIX_NL = ':a;N;$$!ba; s~\r~~g;'
 
-# sed_cmd _REGEX_ESCAPE_SPACE_REPL
-_REGEX_ESCAPE_SPACE_REPL = 's~ ~\\ ~g;'
-
 # sed_cmd _REGEX_GRADLE_REPL(key, value)
 _REGEX_GRADLE_REPL = 's~^\([ \t]*$(1) *= *['\''"]\)[^'\''"]*~\1$(2)~g;'
 
